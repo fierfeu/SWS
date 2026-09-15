dd93633b6eb270025f62e35cef2f2c66d44d2a99
fierfeu <fierfeu@gmail.com>
2026-09-15 11:22:01 +0200

    improve targetDefinition test and source code

--- dev/test/targetDefinition_test.mjs
+++ dev/test/targetDefinition_test.mjs
@@ -1,39 +1,45 @@
-'uses strict'
+'use strict'
 
 import {expect} from 'chai'
 import targetDefinition from '../../src/targetDefinition.mjs';
 
 describe("targetDefinition is working well",()=>{
+    const sitePagesConf = {
+        '/' : '/../web/html/index.html',
+        '/index.html' : '/../web/html/index.html',
+        '/admin.html' : '/../web/html/admin.html'
+    };
+
     // targetDefinition return the good target for a given request.url
-    it("targetDefinition returns the good target for a given url", ()=>{
-        const sitePagesConf = {
-            '/' : 'index.html'
-        };
-        
+    it("returns the good target for a given url", ()=>{
         const url = '/';
         
-        expect(targetDefinition.resolved(url,sitePagesConf)).to.be.equal('index.html');
+        expect(targetDefinition.resolved(url,sitePagesConf)).to.be.equal('/../web/html/index.html');
     });
 
     // query don't change the target
-    it("targetDefinition still return the good target even if there's a query",()=>{
-        const sitePagesConf = {
-            '/' : 'index.html'
-        };
+    it("still returns the good target even if there's a query",()=>{      
+        const url = '/index.html?foo=bar';
         
-        const url = '/?foo=bar';
-        
-        expect(targetDefinition.resolved(url,sitePagesConf)).to.be.equal('index.html');
+        expect(targetDefinition.resolved(url,sitePagesConf)).to.be.equal('/../web/html/index.html');
     });
 
     // hash don't change the target
-    it("targetDefinition still return the good target even if there's a hash in url",()=>{
-        const sitePagesConf = {
-            '/' : 'index.html'
-        };
+    it("still returns the good target even if there's a hash in url",()=>{        
+        const url = '/admin.html#foo';
         
-        const url = '/#foo';
+        expect(targetDefinition.resolved(url,sitePagesConf)).to.be.equal('/../web/html/admin.html');
+    });
+
+    it("should resolve normalized paths with dot segments", () => {
+     expect(targetDefinition.resolved('/admin/../index.html', sitePagesConf)).to.equal('/../web/html/index.html');
+    });
+
+    //returns undefined if the url is not in the sitePagesConf or not valid
+    it("returns undefined if the url is not in the sitePagesConf or not valid",()=>{
+        const url = '/notfound.html';
         
-        expect(targetDefinition.resolved(url,sitePagesConf)).to.be.equal('index.html');
+        expect(targetDefinition.resolved(url,sitePagesConf)).to.be.undefined;
+        expect(targetDefinition.resolved(undefined,sitePagesConf)).to.be.undefined;
     });
 });
--- src/targetDefinition.mjs
+++ src/targetDefinition.mjs
@@ -1,11 +1,19 @@
 'use strict'
+const DUMMY_BASE = "https://localhost:9443"; // base URL used to parse relative paths
 
 export default {
     resolved : (path,pagesConfiguration) => {
-        const result = new URL(path, "http://vvgc.ser")
-        if (pagesConfiguration.hasOwnProperty(result.pathname)) {
-            return pagesConfiguration[result.pathname];
+        try {
+            const result = new URL(path, DUMMY_BASE)
+            if (pagesConfiguration.hasOwnProperty(result.pathname)) {
+                return pagesConfiguration[result.pathname];
+            }
+            console.error("Target Definition : undefined target key but code 200 OK : how can it be possible ?");
+            return undefined;
         }
-        //else console.error("Target DEfinition : undefined target key but code 200 OK : how can it be possible ?");
+        catch {
+            console.error("Target Definition : invalid target key but code 200 OK : how can it be possible ?");
+            return undefined;
+        }
     }
 }
